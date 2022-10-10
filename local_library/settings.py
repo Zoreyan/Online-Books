c617fef386b06eacaf411addd6edbdc2e2e8587d
Zoi <klovvergg@gmail.com>
2022-10-10 20:37:13 +0600

    changes for heroku

--- local_library/settings.py
+++ local_library/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^j=-egc5g4_y3opgkp*&1!#$&nbn5t-etdx4q+gww0e*-=#+p+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-online-books.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
